19e22c33b7f5a8fb16468a06729bfb3222f1f1b1
Tch1b0 <Jpour3006@gmail.com>
2021-06-16 20:24:44 +0200

    main: added a help command and the selection of a algorithm

--- __main__.py
+++ __main__.py
@@ -1,10 +1,20 @@
+from field import Field
 import pygame
 from pygame.constants import QUIT
 import utils
 import random
+import sys
 
-items = [i for i in range(500)]
-random.shuffle(items)
+try:
+    arg = sys.argv[1]
+except:
+    arg = "bubble"
+
+if arg == "-h" or arg == "--help": quit(utils.help())
+algorithm = utils.ALGORITHMS[arg]
+
+field = Field(algorithm)
+field.shuffle()
 
 window = pygame.display.set_mode([500, 500])
 clock = pygame.time.Clock()
@@ -15,9 +25,9 @@ while True:
             quit(pygame.quit())
 
     window.fill((0, 0, 0))
-
-    items = utils.bubble_sort(items)
-    utils.draw(window, items)
+    
+    field.sort(steps=1)
+    utils.draw(window, field.field, field.is_sorted())
 
     pygame.display.update()
     clock.tick(60)
