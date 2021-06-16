063c5a189de85e05abe32652a3bd34ddbe69b0d4
Tch1b0 <Jpour3006@gmail.com>
2021-06-16 22:48:59 +0200

    algorithms: added gnome algorithm

--- util/algorithms.py
+++ util/algorithms.py
@@ -23,7 +23,22 @@ def bogo_sort(items: list) -> list:
     random.shuffle(items)
     return items
 
+def gnome_sort(items: list) -> list:
+    for i in range(len(items) - 1):
+        item_index = i+1
+        if items[item_index] < items[i]:
+            item = items[item_index]
+            for n in range(1, len(items)):
+                new_index = item_index - n
+                if items[new_index+1] > item and items[new_index-1] < item:
+                    print(items.pop(item_index))
+                    items.insert(new_index, item)
+                    return items
+
+    return items
+
 ALGORITHMS = {
     "bubble": bubble_sort,
-    "bogo": bogo_sort
+    "bogo": bogo_sort,
+    "gnome": gnome_sort
 }
